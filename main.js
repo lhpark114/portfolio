ec2f8356ca58a731b017e70b42512b0fc41e58b7
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-02-22 09:47:06 -0500

    JS: Navbar menu toggle + project ui tweak

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ document.addEventListener('scroll', () => {
 });
 
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -23,9 +24,16 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null ) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
 
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
