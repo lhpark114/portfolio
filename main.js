c8d562e0da28c3f5f6b3128c9e73e5bea04dcd08
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-02-22 09:39:08 -0500

    JS: remove active from nav

--- main.js
+++ main.js
@@ -67,8 +67,15 @@ workBtnContainer.addEventListener('click', (e) => {
   if(filter == null) {
     return;
   }
-  projectContainer.classList.add('anim-out');
 
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode; 
+  target.classList.add('selected');
+
+  projectContainer.classList.add('anim-out');
 
   setTimeout(() => {
     projects.forEach((project) => {
